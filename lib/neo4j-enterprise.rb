c02c090bd8e4dc52f9167ccab8e8e0c8243d139d
Dmytrii Nagirniak <dnagir@gmail.com>
2012-01-09 22:05:08 +1100

    added the require code

--- lib/neo4j-enterprise.rb
+++ lib/neo4j-enterprise.rb
@@ -2,6 +2,18 @@ require "neo4j-enterprise/version"
 
 module Neo4j
   module Enterprise
-    # Your code goes here...
+
+    def self.jars_root
+      "#{File.dirname(__FILE__)}/neo4j-enterprise/jars"
+    end
+
+    def self.load_jars!
+      require 'java'
+      puts jars_root
+      Dir["#{jars_root}/*.jar"].each {|jar| require jar }
+    end
+
   end
 end
+
+Neo4j::Enterprise.load_jars!
---
+++ lib/neo4j-jars.rb
@@ -0,0 +1,2 @@
+require 'neo4j-enterprise'
+
