dd14ef9281bfda7c38a0e7805595d0fff27f731f
Andreas Ronge <andreas.ronge@gmail.com>
2012-01-12 15:42:02 +0100

    Release 1.6.0.alpha.1 Added missing apache log4j file, needed to run the HA cluster example

--- lib/neo4j-enterprise/version.rb
+++ lib/neo4j-enterprise/version.rb
@@ -1,12 +1,14 @@
 module Neo4j
   module Enterprise
-    VERSION = "1.6.0.alpha.3"
+    VERSION = "1.6.0.alpha.1"
     NEO_VERSION = "1.6.M02"
   end
 
   # make sure community, advanced and enterprise neo4j jar files have the same version
-  if defined?(NEO_VERSION) && NEO_VERSION != Enterprise::NEO_VERSION
-    raise "Mismatch of Neo4j JAR versions. Already loaded JAR files #{NEO_VERSION}, neo4j-enterprise: #{Enterprise::NEO_VERSION}" 
+  if defined?(NEO_VERSION)
+    if NEO_VERSION != Enterprise::NEO_VERSION
+      raise "Mismatch of Neo4j JAR versions. Already loaded JAR files #{NEO_VERSION}, neo4j-enterprise: #{Enterprise::NEO_VERSION}" 
+    end
   else
     NEO_VERSION = Enterprise::NEO_VERSION
   end
