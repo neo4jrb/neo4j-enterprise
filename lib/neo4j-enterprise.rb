2de7796af91ccb7cfa8ae8c009015589b98bc0ef
Volker Pacher <vpacher@ebay.com>
2015-04-30 15:26:30 +0100

    add all jars to gem

--- lib/neo4j-enterprise.rb
+++ lib/neo4j-enterprise.rb
@@ -2,18 +2,23 @@ require "neo4j-enterprise/version"
 
 module Neo4j
   module Enterprise
-
     def self.jars_root
       File.join("#{File.dirname(__FILE__)}", "neo4j-enterprise", "jars")
     end
 
+    def self.test_jars_root
+      File.join("#{File.dirname(__FILE__)}", "neo4j-enterprise", "test-jars")
+    end
+
     def self.load_jars!
       require 'java'
-      require 'neo4j-community'
-      ::Neo4j::Community.ensure_version!(Enterprise::NEO_VERSION, 'enterprise')
       Dir["#{jars_root}/*.jar"].each {|jar| require jar }
     end
 
+    def self.load_test_jars!
+      require 'java'
+      Dir["#{test_jars_root}/*.jar"].each {|jar| require jar }
+    end
   end
 end
 
